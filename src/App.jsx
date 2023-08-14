bcc2f1028c29498a0196490eaf530b7061cccaee
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-08-14 15:09:37 -0300

    Adicionando PageDicas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './layout/Layout';
 import { HomePage } from './pages/HomePage/HomePage'
 import { LoginPage } from './pages/LoginPage/LoginPage';
+import { DicasPage } from './pages/DicasPage/DicasPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Layout/>} >
             
             <Route path='/home' element={<HomePage/>}/>
+            <Route path='/dicas' element={<DicasPage/>}/>
 
         </Route>
         
